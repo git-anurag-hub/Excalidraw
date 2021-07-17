b01dbcc5d7d3044ab8356af4f0a8f4c515b2d41f
git-anurag-hub <aguptaking@gmail.com>
2021-07-17 15:33:14 +0530

    Added Canvas

---
+++ src/components/Canvas.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState, useRef } from "react";
+import Excalidraw from "@excalidraw/excalidraw";
+
+const Canvas = () => {
+  const excalidrawRef = useRef(null);
+  const [viewModeEnabled, setViewModeEnabled] = useState(false);
+  const [zenModeEnabled, setZenModeEnabled] = useState(false);
+  const [gridModeEnabled, setGridModeEnabled] = useState(false);
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <div>
+      <div class="m-10 flex justify-center">
+        <button
+          className={`border rounded py-1 px-3 text-white m-2 ${
+            viewModeEnabled ? "bg-green-500" : "bg-gray-300"
+          }`}
+          onClick={() => setViewModeEnabled(!viewModeEnabled)}
+        >
+          View Mode
+        </button>
+        <button
+          className={`border rounded py-1 px-3 text-white m-2 ${
+            zenModeEnabled ? "bg-green-500" : "bg-gray-300"
+          }`}
+          onClick={() => setZenModeEnabled(!zenModeEnabled)}
+        >
+          Zen Mode
+        </button>
+        <button
+          className={`border rounded py-1 px-3 text-white m-2 ${
+            gridModeEnabled ? "bg-green-500" : "bg-gray-300"
+          }`}
+          onClick={() => setGridModeEnabled(!gridModeEnabled)}
+        >
+          Grid mode
+        </button>
+        <button
+          className={`border rounded py-1 px-3 m-2 ${
+            theme == "light" ? "bg-white text-black" : "bg-black text-white"
+          }`}
+          onClick={() => {
+            if (theme == "light") {
+              setTheme("dark");
+            } else {
+              setTheme("light");
+            }
+          }}
+        >
+          Theme
+        </button>
+        <button
+          className="border rounded py-1 px-3 bg-red-500 text-white m-2"
+          onClick={() => {
+            excalidrawRef.current.resetScene();
+          }}
+        >
+          Reset
+        </button>
+      </div>
+
+      <div class="border-gray-900 border rounded-lg m-10" style={{ height: "800px" }}>
+        <Excalidraw
+          ref={excalidrawRef}
+          onChange={(elements, state) => console.log("Elements :", elements, "State : ", state)}
+          onCollabButtonClick={() => window.alert("Not Implemented!")}
+          viewModeEnabled={viewModeEnabled}
+          zenModeEnabled={zenModeEnabled}
+          gridModeEnabled={gridModeEnabled}
+          theme={theme}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Canvas;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header class="flex justify-center text-4xl mt-10 font-bold">Enthire - ExcaliDraw</header>
+    </div>
+  );
+};
+
+export default Header;
